d847b366bf1ecc08860efa58c25124505d3a81a4
bcurl <bcurl@nd.edu>
2012-11-15 18:28:03 -0500

    Update plugin2/chrome/content/graphicalInterface.js

--- plugin2/chrome/content/graphicalInterface.js
+++ plugin2/chrome/content/graphicalInterface.js
@@ -28,10 +28,34 @@ function addItemToMenu(id, label, command, menuid, insertBefore){
 function loadSavedPages(){
 	addItemToMenu('test', 'Click me', null, 'pagesavermenupopup', 'testextension-menuitem');
 }
+function saveSomething(){
+	var savedName = document.getElementById("pageDescription").value;
+	Components.utils.reportError("selected page is: " + savedName);
+	newFolder('teste rs');
+	storePage(savedName, '871122613', content);
+	//var urll = retrievePage('848722047', '105704317');
+	//Components.utils.reportError(urll);
+	//gBrowser.addTab(urll);
+		
+	//deletePage('871122613', '12370611');
+	//deleteFolder('871122613');
+	
+	
+}
+
+function onWindowLoad(){
+	for(var i =0;i<window.arguments[0].length;i++){
+		document.getElementById("folderList").appendItem(window.arguments[0][i]);
+	}
+}
 
 function savePage(){
 	try {
-		//window.open("chrome://pagesaver/content/savePage.xul", "bmarks", "chrome,width=600,height=300");
+		var myArray = new Array();
+		myArray[0]= "Apples";
+		myArray[1]= "Oranges";
+		myArray[2]= "Bananas";
+		window.openDialog("chrome://pagesaver/content/savePage.xul", "bmarks", "chrome,width=600,height=50,centerscreen",myArray);
 		//newFolder('teste rs');
 		//storePage('rio grande do sul', '871122613', content);
 		//var urll = retrievePage('848722047', '105704317');
@@ -39,7 +63,7 @@ function savePage(){
 		//gBrowser.addTab(urll);
 		
 		//deletePage('871122613', '12370611');
-		deleteFolder('871122613');
+		//deleteFolder('871122613');
 	} catch (err){
 		Components.utils.reportError(err);
 		Components.utils.reportError(err.message);
